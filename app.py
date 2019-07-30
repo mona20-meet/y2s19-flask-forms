5d3a46ae169f0abb1111b71176f2847a170da93b
myname21-meet <myname21@meet.mit.edu>
2019-07-30 11:28:31 -0400

    fref

--- app.py
+++ app.py
@@ -12,5 +12,11 @@ def display_student(student_id):
 
 #Create an '/add' route here:
 
+@app.route('/add')
+def add_student-route():
+    return render_template('add.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
